5e408f47a68ca8c1cf0e64917111d8ce8d6498ad
Ralph <devnymeria@gmail.com>
2023-06-17 11:10:05 -0500

    modelado

--- domain/model/src/main/java/co/com/atm/model/account/Account.java
+++ domain/model/src/main/java/co/com/atm/model/account/Account.java
@@ -1,9 +1,13 @@
 package co.com.atm.model.account;
+import co.com.atm.model.transaction.Transaction;
 import lombok.Builder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Getter
 @Setter
@@ -11,4 +15,8 @@ import lombok.Setter;
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class Account {
+    private Long id;
+    private String accountNumber;
+    private BigDecimal balance;
+    private List<Transaction> transactionHistory;
 }
--- domain/model/src/main/java/co/com/atm/model/transaction/Transaction.java
+++ domain/model/src/main/java/co/com/atm/model/transaction/Transaction.java
@@ -1,14 +1,23 @@
 package co.com.atm.model.transaction;
+import co.com.atm.model.account.Account;
+import co.com.atm.model.transactiontype.TransactionType;
 import lombok.Builder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-//import lombok.NoArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Getter
 @Setter
-//@NoArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class Transaction {
+    private Long id;
+    private TransactionType transactionType;
+    private BigDecimal amount;
+    private BigDecimal finalBalance;
+    private Account account;
 }
