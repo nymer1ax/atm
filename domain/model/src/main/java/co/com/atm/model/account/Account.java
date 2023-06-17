5c911aac3a3e62d08b005c75ef0261f38cbe4493
Ralph <devnymeria@gmail.com>
2023-06-17 10:59:33 -0500

    wrapper

--- domain/model/src/main/java/co/com/atm/model/account/Account.java
+++ domain/model/src/main/java/co/com/atm/model/account/Account.java
@@ -2,12 +2,12 @@ package co.com.atm.model.account;
 import lombok.Builder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-//import lombok.NoArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-//@NoArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class Account {
